539e7f2852e3d95a215f6ef36da8d444d567324a
Alexandr Trifonov <ast89646421790@yandex.ru>
2024-10-26 19:34:50 +0300

    first test

---
+++ src/test/java/HeaderTest.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+import java.time.Duration;
+
+public class HeaderTest {
+
+    @Test
+    public void test() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        textBox.sendKeys("Selenium");
+
+        WebElement textArea = driver.findElement(By.name("my-textarea"));
+        textArea.sendKeys("Alex!");
+
+        WebElement submitButton = driver.findElement(By.className("btn"));
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+
+        Assert.assertEquals(value, "Received!");
+
+        driver.quit();
+    }
+}
