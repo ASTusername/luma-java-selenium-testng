50d19ef1ca312fecf12d1505132ea5d4c3e67bc2
Alex <2231790@gmail.com>
2025-08-07 21:21:06 +0300

    настройки selenoid

--- src/test/java/runner/BaseTest.java
+++ src/test/java/runner/BaseTest.java
@@ -1,9 +1,14 @@
 package runner;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public abstract class BaseTest {
 
@@ -12,6 +17,17 @@ public abstract class BaseTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://magento.softwaretestingboard.com/";
         Configuration.pageLoadStrategy = "eager";
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @AfterEach
